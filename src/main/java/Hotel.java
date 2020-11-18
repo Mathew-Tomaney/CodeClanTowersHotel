c3ff5be1ac5a313e6c22c7f9b81170fb6c9dd489
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-11-18 14:38:03 +0000

    added add room fns to hotel class and passed tests. Added canCheckinGuestToRoom to hotel tests

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
 public class Hotel {
+    private ArrayList<Bedroom> bedrooms;
+    private ArrayList<ConferenceRoom> conferenceRooms;
+
+    public Hotel(){
+        this.bedrooms = new ArrayList<Bedroom>();
+        this.conferenceRooms = new ArrayList<ConferenceRoom>();
+    }
+    public int getBedroomCount(){
+        return this.bedrooms.size();
+    }
+    public int getConferenceCount(){
+        return this.conferenceRooms.size();
+    }
+    public void addBedroom(Bedroom bedroom){
+        this.bedrooms.add(bedroom);
+    }
+    public void addConference(ConferenceRoom conferenceRoom){
+        this.conferenceRooms.add(conferenceRoom);
+    }
+
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -7,24 +7,34 @@ public class HotelTest {
     private Hotel hotel;
     private Guest guest;
     private Bedroom bedroom;
+    private Bedroom testBedroom;
     private ConferenceRoom conferenceRoom;
+    private ConferenceRoom testConferenceRoom;
+
 
     @Before
     public void before(){
         hotel = new Hotel();
         guest = new Guest("Graeme Graemeson");
         bedroom = new Bedroom(2, 3, "Single");
+        testBedroom = new Bedroom(1, 2, "Double");
         conferenceRoom = new ConferenceRoom("The Tundra", 9);
+        testConferenceRoom = new ConferenceRoom("Hopper", 5);
     }
     @Test
     public void canAddBedroom(){
-        hotel.addBedroom();
+        hotel.addBedroom(testBedroom);
         assertEquals(1, hotel.getBedroomCount());
     }
     @Test
     public void canAddConferenceRoom(){
-        hotel.addConference();
+        hotel.addConference(testConferenceRoom);
         assertEquals(1, hotel.getConferenceCount());
+    }
+    @Test
+    public void canCheckGuestInToBedroom(){
+        hotel.checkInGuest(guest, bedroom);
+        assertEquals(true, hotel.checkRoomOccupied());
     }
 
 }
